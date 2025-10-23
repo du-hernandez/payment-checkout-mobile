7abfc733bc08d6c5b0132119485581882700521e
Duverney Hernandez Mora <du.hernandez@udla.edu.co>
2025-10-23 15:03:58 -0500

    feat (splash-screen): installation and configuration for android of react-native-bootsplash

--- App.tsx
+++ App.tsx
@@ -6,13 +6,28 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
+import { useEffect } from 'react';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
+import BootSplash from "react-native-bootsplash";
+
 function App() {
+
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+      console.log("BootSplash has been hidden successfully");
+    });
+  }, []);
+
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
